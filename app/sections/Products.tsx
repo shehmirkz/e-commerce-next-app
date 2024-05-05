2826014aac2f81c7a1de6c5d1ca51057c4ff354d
Shahmir Hussain <shahmir.hussain.khanzada@gmail.com>
2024-05-06 00:51:27 +0500

    refactor: dynamic slug

--- app/sections/Products.tsx
+++ app/sections/Products.tsx
@@ -5,6 +5,8 @@ import ProductCard from "../components/ProductCard";
 import { productCardData } from "../data";
 
 import { useQuery, gql } from "@apollo/client";
+import { useRouter, usePathname } from "next/navigation";
+import Link from "next/link";
 
 function Products() {
   const GET_NAV_ITEMS = gql`
@@ -31,12 +33,15 @@ function Products() {
         <div className="flex lg:justify-between">
           <div className="flex items-center gap-[30px]">
 
-            <button className="font-bold  text-[#1E2832] text-[16px]">
+            <Link href="#all-products" className="font-bold  text-[#1E2832] text-[16px]">
               All Products
-            </button>
+            </Link>
 
             {data.tags.nodes.map(({ name, displayTitle, slug }: any) => (
-              <button className="text-[#1E2832] text-[16px]">{displayTitle}</button>
+              <Link href={{
+                pathname: "/",
+                query: { tag: slug}
+              }} className="text-[#1E2832] text-[16px]">{displayTitle}</Link>
             ))}
           </div>
 
@@ -62,69 +67,3 @@ function Products() {
 }
 
 export default Products;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <div>
-//   {data.tags.nodes.map(({ name, displayTitle, slug }: any) => (
-//     <div key={name}>
-//       <h3>{name}</h3>
-//       <img
-//         width="400"
-//         height="250"
-//         alt="location-reference"
-//         src={`${slug}`}
-//       />
-//       <br />
-//       <b>About this location:</b>
-//       <p>{displayTitle}</p>
-//       <br />
-//     </div>
-//   ))}
-// </div>
-
-// return (
-
-{
-  /* <section className="my-[40px] mx-auto container">
-  <div className="flex lg:justify-between">
-    <div className="flex items-center gap-[30px]">
-      <button className="font-bold  text-[#1E2832] text-[16px]">
-        All Products
-      </button>
-      <button className="text-[#1E2832] text-[16px]">t-shirt</button>
-      <button className="text-[#1E2832] text-[16px]">Hoodies</button>
-      <button className="text-[#1E2832] text-[16px]">Jacket</button>
-    </div>
-    <PrimaryButton
-      name="filter"
-      class="bg-[#1e2832] text-white"
-      iconItem={faFilter}
-    />
-  </div>
-  <div className="flex flex-wrap p-[6px] m-4 mt-[40px]">
-    {productCardData.map((cardData) => (
-      <div
-        className="lg:w-1/4 md:w-1/2 px-2 w-full mb-5 hover:shadow-2xl duration-200 hover:mt-[-10px] py-2"
-        key={cardData.id}
-      >
-        <ProductCard {...cardData} />
-      </div>
-    ))}
-  </div>
-</section>; */
-}
-
-// )
